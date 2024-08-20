d03211c8e7a5da84d826238f8cd188b5bcc1942e
Laion Ubert <laiondevubert@gmail.com>
2024-08-20 10:42:35 -0300

    Leitura de lista de endereços de email por arquivo

--- main.py
+++ main.py
@@ -10,17 +10,28 @@ smtp_server = "INSERT SMTP ADDRESS"
 login = "INSERT EMAIL ADDRESS"
 password = "INSERT PASSWORD" 
 
-# Leitura do arquivo html
-with open('EMAIL_HTML_FILE', 'rb') as file:
-    file_content = file.read().decode('utf-8', 'ignore')
 
-with open('EMAIL_ATTACHMENT', 'rb') as file:
-    attachment = file.read()
-    
-    
+#Configuracao dos enderecos de email
 emailRemetente = "EMAIL SENDER"
 emailDestinatario = "EMAIL DESTINATION"
 emailCopiaOculta = ["LIST EMAIL HIDDEN ADDRESSES"]
+
+# Leitura de arquivos
+## leitura do arquivo html
+with open('EMAIL_HTML_FILE', 'rb') as file:
+    file_content = file.read().decode('utf-8', 'ignore')
+
+## leitura do arquivo de anexo
+with open('EMAIL_ATTACHMENT', 'rb') as file:
+    attachment = file.read()
+
+## leitura da lista de enderecos de envio
+with open("mail_list.txt") as file:
+    for line in file :
+        if len(line.strip()) > 0:
+            emailCopiaOculta.append(line.strip())
+    
+
 # Composicao do cabecalho do e-mail
 message = MIMEMultipart("alternative")
 message["Subject"] = "INSERT SUBJECT"
@@ -29,12 +40,14 @@ message["To"] = emailDestinatario
 message["Bcc"] = ', '.join(emailCopiaOculta)
 
 
+#
 part1 = MIMEText(file_content, 'html')
 message.attach(part1)
+
 
 # Envio do e-mail
 with smtplib.SMTP("SMTP ADDRESS", 587) as server:
     server.login(login, password)
     server.sendmail(emailRemetente, emailCopiaOculta, message.as_string())
 
-print('enviado')
+print('E-mail Enviado')
